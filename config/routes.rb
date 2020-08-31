100f2071fb6a597429a0a1401327d3200ae03287
Mohamed Soliman <mosoliman4@gmail.com>
2020-08-31 17:19:20 +0200

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+  resources :jamsessions do
+    resources :posts, only: [:index, :create, :destroy, :new] do
+      member do
+        put :accept
+        put :decline
+      end
+    end
+  end
+  resources :instruments
+  resources :reviews, only: [:index, :new, :create]
+
 end
