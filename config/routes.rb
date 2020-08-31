2ea1886a251236d8e1a57e183663a84e14ddb606
Mohamed Soliman <mosoliman4@gmail.com>
2020-08-31 17:58:02 +0200

    requests/routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'pages#home'
   resources :jamsessions do
-    resources :posts, only: [:index, :create, :destroy, :new] do
+    resources :requests, only: [:new, :create, :delete, :index] do
       member do
         put :accept
         put :decline
       end
+      
     end
+    resources :posts, only: [:index, :create, :destroy, :new]
   end
   resources :instruments
   resources :reviews, only: [:index, :new, :create]
+  
 
 end
