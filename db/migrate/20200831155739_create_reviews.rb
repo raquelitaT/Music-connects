18be388cb0a391b33f630a0fcd03866340060e9f
Addison Holbrook <holbrooa@gmail.com>
2020-09-01 10:21:17 +0200

    models

--- db/migrate/20200831155739_create_reviews.rb
+++ db/migrate/20200831155739_create_reviews.rb
@@ -1,7 +1,7 @@
 class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
-      t.sting :content
+      t.string :content
       t.integer :rating
 
       t.timestamps
