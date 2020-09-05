ab304848f15e78fe28184ff3072724dfe70c1cc2
Laura van Nieuwland <lauravannieuwland@gmail.com>
2020-09-05 15:43:00 +0200

    Add user instruments on profile page incl skill level

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @host_user_requests = Request.where(jam_session_id: current_user.jam_sessions)
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
   end
 
 end
