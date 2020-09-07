7081813f9d833cf4f2b88256b2e4827ae8d5c6cd
Raquel Tessarollo <tessarollo.raquel@gmail.com>
2020-09-07 10:16:15 +0200

    Add style to mapbox

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -6,7 +6,7 @@ const buildMap = () => {
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   return new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10'
+    style: 'mapbox://styles/raqueltessarollo/cker5flfe5skb19o22xlaqum0'
   });
 };
 
